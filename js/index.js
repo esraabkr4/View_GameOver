636de85deb80d26cc19b74478ef7ade3a572eb66
esraabkr4 <esraabkr4@gmail.com>
2024-07-04 13:05:15 +0300

    first commit

--- js/index.js
+++ js/index.js
@@ -1,90 +1,133 @@
 'use strict';
-const api_key = 'f7fdd6aa792047a382d195611242206';
-const weather_days = '2';
-let search_input = document.getElementById("search_input");
-let search_button = document.getElementById("search_button");
-let row = document.getElementById("row");
-let search_form = document.forms[0];
-let weather_arr = [];
-const dayName = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-const day = dayName[new Date().getDay()];
-const formattedDate = new Date().toLocaleDateString('en-GB', {
-  day: 'numeric', month: 'long'
-}).replace(/ /g, '');
-view_weather();
+let Games = document.getElementById("Games");
+let GameDetails = document.getElementById("GameDetails");
+let navLinks = document.querySelectorAll(".nav-link");
+let tabPane = document.querySelector(".tab-pane");
+let row = document.querySelector(".row");
+let details = document.querySelector(".details");
+let loader = document.getElementById("loader");
+let array = [];
+const base_url = 'https://www.freetogame.com/api';
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "b7ddeaba2emsh4c7f9a279de19b4p17f62fjsn9ef33d17e7d9",
+    "X-RapidAPI-Host": "free-to-play-games-database.p.rapidapi.com",
+  },
+};
+getGames('mmorpg').then(result => { loader.classList.remove("loader"); display(result) });
+
+class gamesClass {
+  constructor(thumbnail, title, genre, platform, id, short_description) {
+    this.id = id;
+    this.thumbnail = thumbnail;
+    this.title = title;
+    this.short_description = short_description;
+    this.genre = genre;
+    this.platform = platform;
+  }
+
+}
+class detailsClass extends gamesClass {
+  constructor(thumbnail, title, genre, platform, status, description, game_url) {
+    super(thumbnail, title, genre, platform);
+    this.status = status;
+    this.description = description;
+    this.game_url = game_url;
+  }
+
+}
 
 
-search_form.addEventListener("submit", function (e) {
-  e.preventDefault();
-  view_weather();
 
+navLinks.forEach(element => {
+  element.addEventListener("click", async function () {
+    let category = element.getAttribute("data-category");
+    tabPane.setAttribute("id", "pills-" + category);
+    tabPane.setAttribute("aria-labelledby", "pills-" + category + "-tab");
+    tabPane.classList.add("active", "show");
+    await getGames(category).then(result => { loader.classList.remove("loader"); display(result) });
+
+  })
 });
 
-
-async function view_weather(country){
-  weather_arr = await search_fun(search_input.value);
-  display_fun();
+async function getGames(categoreOption) {
+  loader.classList.add("loader");
+  const api = await fetch(
+    `https://free-to-play-games-database.p.rapidapi.com/api/games?category=${categoreOption}`,
+    options
+  );
+  return await api.json();
 }
 
 
-async function search_fun(country) {
-  if (!country) {
-    country = 'Cairo';
-  }
-  let url = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=${api_key}&q=${country}&days=${weather_days}`);
-  return await url.json();
-}
-
-
-function display_fun() {
-  let { location, current, forecast } = weather_arr;
-  let { temp_c, condition } = current;
-  let { icon, text } = condition;
-
-  var box = `<div class="col-md-4 shadow-lg">
-            <div class="card text-white card_one">
-                <div class="card-header d-flex justify-content-between card_two">
-                  <small class="text-secondary">${day}</small>
-                  <small class="text-secondary">${formattedDate}</small>
-                </div>
+function display(gamesArr) {
+  let box = "";
+  for (let i = 0; i < gamesArr.length; i++) {
+    let game = new gamesClass(gamesArr[i].thumbnail, gamesArr[i].title, gamesArr[i].genre, gamesArr[i].platform, gamesArr[i].id, gamesArr[i].short_description);
+    box += `<div class="col-lg-3 col-md-4 gameCard" onclick="getDetails(${game.id})">
+              <div class="card p-3">
+                <img src="${game.thumbnail ? game.thumbnail : "images/placeholder-614.webp"}" class="card-img-top" alt="game img">
                 <div class="card-body">
-                  <h4 class="opacity-75">${location?.name}</h4>
-                  <h5 class="card-title display-1 fw-bolder">${temp_c} oc</h5>
-                  <img src="https:${icon}" class="card-img-top w-25 h-25" alt="...">
-                  <span class="text-info">${text}</span>
-                  <!-- <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p> -->
+                <div class="d-flex justify-content-between align-items-center">
+                  <h5 class="card-title text-white">${game.title}</h5>
+                  <span class="badge text-bg-primary p-2">Free</span>
+                  </div>
+                  <p class="card-text text-secondary fw-medium">
+                  ${game.short_description.split(" ", 8).join(",")}
+                 </p>
                 </div>
                 <div class="card-footer d-flex justify-content-between">
-                  <small class="text-secondary"><i class="fa-solid fa-umbrella"></i> 20%</small>
-                  <small class="text-secondary"><i class="fa-solid fa-wind"></i> 18km/h</small>
-                  <small class="text-secondary"><i class="fa-solid fa-compass"></i> East</small>
+                  <small class="text-white badge bg-dark">${game.genre}</small>
+                  <small class="text-white badge bg-dark">${game.platform}</small>
                 </div>
               </div>
-        </div>`
-  console.log(typeof (forecast))
-  forecast?.forecastday.forEach((element,index) => {
-    let { day } = element;
-    let { maxtemp_c, mintemp_c, condition } = day;
-    let { icon, text } = condition;
-    box += `<div class="col-md-4 shadow-lg">
-            <div class="card text-white px-0 d-flex align-items-center ${(index%2==0)?"card_two":"card_one"}">
-                <div class="card-header card_two w-100 text-center">
-                  <small class="text-secondary">${dayName[new Date().getDay() + (index+1)]}</small>
-                </div>
-                <div class="card-body">
-                <img src="https:${icon}" class="card-img-top w-50 h-50" alt="...">
 
-                  <h5 class="card-title display-6 fw-bolder">${maxtemp_c} oC</h5>
-                  <h5 class="card-title display-6">${mintemp_c} o</h5>
-                  <span class="text-info">${text}</span>
-                  <!-- <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p> -->
-                </div>
-                
-              </div>
-        </div>`
-  });
-
+          </div>`
+  }
   row.innerHTML = box;
+  console.log(row);
+
 }
 
-// get user current location
+// -- details 
+
+async function getDetails(id) {
+  loader.classList.add("loader");
+  Games.classList.replace("d-block", "d-none");
+  GameDetails.classList.replace("d-none", "d-block");
+  const api = await fetch(
+    `https://free-to-play-games-database.p.rapidapi.com/api/game?id=${id}`,
+    options
+  );
+  const responseData = await api.json().then(loader.classList.remove("loader"));
+  let detail = new detailsClass(responseData.thumbnail, responseData.title, responseData.genre, responseData.platform, responseData.status, responseData.description, responseData.game_url);
+  console.log(responseData);
+  console.log(detail);
+  details.innerHTML = `
+  <div class="d-flex justify-content-between my-5">
+  <h2 class="text-white display-4">Details Game</h2>
+            <button type="button" class="btn-close text-white bg-white" aria-label="Close" onclick="closeDetails()"></button>
+</div>
+      <div class="row">
+      <div class="col-md-4">
+        <img src="${detail.thumbnail}" class="w-100" alt="game img" />
+      </div>
+      <div class="col-md-8">
+        <ul class="list-unstyled">
+          <li class="h3 text-white">Title: <span class="badge text-bg-info text-black">${detail.title}</span></li>
+          <li class="text-white">Category:  <span class="badge text-bg-info text-black">${detail.genre}</span></li>
+          <li class="text-white">Platform:  <span class="badge text-bg-info text-black">${detail.platform}</span></li>
+          <li class="text-white">Status:  <span class="badge text-bg-info text-black">${detail.status}</span></li>
+        </ul>
+        <p>${detail.description}</p>
+        <a href="${detail.game_url}" type="button" class="btn btn-outline-warning mb-5">Show Games</a>
+
+      </div>
+      </div>`
+};
+
+function closeDetails() {
+  GameDetails.classList.replace("d-block", "d-none");
+  Games.classList.replace("d-none", "d-block");
+}
